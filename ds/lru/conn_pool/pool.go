e35fb38e349a2c131a2fb1505e10e1a8e92ac3ef
rahatbhambri97 <rahat.bhambri@alpha-grep.com>
2025-06-19 23:14:26 +0530

    added env file

--- ds/lru/conn_pool/pool.go
+++ ds/lru/conn_pool/pool.go
@@ -2,18 +2,35 @@ package conn_pool
 
 import (
 	"database/sql"
+	"fmt"
 	"sync"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 )
 
 type DB struct {
 	conn_str string
 }
 
+var dbPass string
+
+func init() {
+	err := godotenv.Load("./vars.env")
+	if err != nil {
+		log.Print(".env file not found")
+	}
+
+	viper.AutomaticEnv()
+	dbPass = viper.GetString("DB_PASSWORD")
+	log.Print("inside init", dbPass)
+}
+
 func CreateDBConn() interface{} {
-	dcs := ""
+	dcs := fmt.Sprintf("root:%s@tcp(127.0.0.1:3306)/sys?parseTime=true&timeout=5s", dbPass)
+	// log.Print(dcs)
 
 	db, err := sql.Open("mysql", dcs)
 	if err != nil {
